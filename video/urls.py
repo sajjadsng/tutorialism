e70c78f8093a31327c90fdc84f86838bd3ba8bba
geek <mohammadpagard.dev@gmail.com>
2023-12-24 18:52:35 +0330

    Add list of short videos endpoint and short video detail endpoint :/

--- video/urls.py
+++ video/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import CourseVideoView
+from .views import CourseVideoView, ShortVideoListView, ShortVideoDetailView
 
 app_name = 'video'
-urlpatterns = [
+course_urlpatterns = [
     path('<int:video_id>/<str:video_slug>/', CourseVideoView.as_view(), name='course_video'),
 ]
+short_urlpatterns = [
+    path('short/', ShortVideoListView.as_view(), name='short_video_list'),
+    path('short/<int:video_id>/<str:video_slug>/', ShortVideoDetailView.as_view(), name='short_video_detail'),
+]
+
+urlpatterns = course_urlpatterns + short_urlpatterns
